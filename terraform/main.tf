4158f9101748c041640fcbdbd2e207530105669f
Joshua Roman <josh.ray.roman@gmail.com>
2022-05-15 10:53:11 -0400

    Josh - remove workspace path from apply and destroy. remove aws instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,8 +9,8 @@ terraform {
     region = "us-east-1"
   }
 }
-resource "aws_instance" "newinstance" {
-  ami           = "ami-0c19f80dba70861db"
-  instance_type = "t2.micro"
+# resource "aws_instance" "newinstance" {
+#   ami           = "ami-0c19f80dba70861db"
+#   instance_type = "t2.micro"
   
-}
+# }

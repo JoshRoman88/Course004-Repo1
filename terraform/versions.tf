dcbe0632c82cf7fe3bca0dbec8bffa1622c15195
Joshua Roman <josh.ray.roman@gmail.com>
2022-06-17 00:47:19 -0400

    Josh - add rand version. update version number

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 3.20.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.3.1"
+    }
     local = {
       source  = "hashicorp/local"
       version = "2.1.0"

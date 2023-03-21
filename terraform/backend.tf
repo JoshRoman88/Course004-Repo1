c898dd24231a4e4d574bfbb7edcf2421a5fbeab6
Josh Roman <94814166+ray-dr@users.noreply.github.com>
2023-03-21 11:04:43 -0400

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 terraform {
     required_providers {
     docker = {
       source  = "kreuzwerker/docker"
       version = "3.0.1"
     }
-    }
+   }
+
+provider "docker" {}
   backend "s3" {
     bucket = "book-mark--989282"
     region = "us-east-1"
